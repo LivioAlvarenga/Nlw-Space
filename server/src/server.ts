2bea721a5b5022537dc2f8341413477af98e3187
Livio Alvarenga <livioalvarenga@gmail.com>
2023-05-20 23:59:40 -0300

    add integration of github and libraries

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import 'dotenv/config'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -7,12 +10,17 @@ const app = fastify()
 app.register(cors, {
   origin: true, // all URLs of the frontend application, be careful with this in production. In production add only the URL to the frontend application
 })
+app.register(jwt, {
+  secret: 'spacetime', // change this in production
+})
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,28 @@
+export function Copyright() {
+  return (
+    <div>
+      <div className="text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="underline hover:text-gray-100"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </div>
+      <div className="text-sm leading-relaxed text-gray-200">
+        🚀 Portfolio{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="underline hover:text-gray-100"
+          href="https://livioalvarenga.com/"
+        >
+          Livio Alvarenga
+        </a>
+      </div>
+    </div>
+  )
+}
